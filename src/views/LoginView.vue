ae9384227f624b77a1bb98e7533e5abc83ba0fd9
Muhammad Fakhrian Ikhwaludin <mdrianid@gmail.com>
2023-05-19 23:36:12 +0700

    img on LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,25 @@
 <script setup>
-import LoginForm from '../components/authentication/LoginForm.vue'
+import LoginForm from "../components/authentication/LoginForm.vue";
 </script>
 
 <template>
-    <main>
-        <div class="relative overflow-hidden bg-white">
-            <div class="mx-auto max-w-7xl">
-                <div class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0">
-                    <div class="w-full p-5 mx-auto sm:max-w-md">
-                        <h2 class="mb-5 text-4xl font-bold text-center text-black">
-                            Welcome Back
-                        </h2>
-                        <LoginForm />
-                    </div>
-                    <div class="w-full mx-auto mt-20 sm:max-w-2xl">
-                        <img src="img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
-                    </div>
-                </div>
-            </div>
+  <main>
+    <div class="relative overflow-hidden bg-white">
+      <div class="mx-auto max-w-7xl">
+        <div
+          class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0"
+        >
+          <div class="w-full p-5 mx-auto sm:max-w-md">
+            <h2 class="mb-5 text-4xl font-bold text-center text-black">
+              Welcome Back
+            </h2>
+            <LoginForm />
+          </div>
+          <div class="w-full mx-auto mt-20 sm:max-w-2xl">
+            <img src="../assets/img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
+          </div>
         </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
